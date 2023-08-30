200e79feff8a8b4a6e7002ddf7249290a985f30b
HamzaFikri <mohammed.hamza.fikri@gmail.comgit config --global user.name HamzaFikrigit config --global user.email mohammed.hamza.fikri@gmail.com>
2023-08-30 13:25:07 -0700

    Header file

--- binary_trees.h
+++ binary_trees.h
@@ -24,6 +24,8 @@ typedef struct binary_tree_s
 
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s heap_t;
 
 /**
 * struct node_list - Linkedlist node
