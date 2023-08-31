85e8b7a93461b66c6f943d5c46a77c39c7d7ce0e
HamzaFikri <mohammed.hamza.fikri@gmail.comgit config --global user.name HamzaFikrigit config --global user.email mohammed.hamza.fikri@gmail.com>
2023-08-31 05:35:53 -0700

    Header file

--- binary_trees.h
+++ binary_trees.h
@@ -1,10 +1,18 @@
-#ifndef _BINARY_TREES_H_
-#define _BINARY_TREES_H_
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
-#include <math.h>
+#include <string.h>
+#include <stdbool.h>
+#include <limits.h>
+
+
+
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+/* basic binary tree */
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -14,44 +22,25 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} binary_tree_t;
+};
 
 typedef struct binary_tree_s binary_tree_t;
+
+/* Binary Search Tree */
 typedef struct binary_tree_s bst_t;
+
+/* AVL Tree */
 typedef struct binary_tree_s avl_t;
+
+/* Max Binary Heap */
 typedef struct binary_tree_s heap_t;
 
-/**
-* struct node_list - Linkedlist node
-*
-* @n: Integer stored in the node
-* @next: Pointer to next node in the list
-**/
-typedef struct node_list
-{
-	int n;
-	struct node_list *next;
-} node_l;
-
-/**
-* struct list_of_list - Linkedlist of linkedlist node
-*
-* @node: Pointer to the node of likendlist
-* @next: Pointer to next node of list pointing to a linkedlist
-**/
-typedef struct list_of_list
-{
-	node_l *node;
-	struct list_of_list *next;
-} list_l;
-
-void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -72,7 +61,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-									const binary_tree_t *second);
+	const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
@@ -88,5 +77,18 @@ avl_t *array_to_avl(int *array, size_t size);
 avl_t *avl_remove(avl_t *root, int value);
 avl_t *sorted_array_to_avl(int *array, size_t size);
 int binary_tree_is_heap(const binary_tree_t *tree);
+heap_t *heap_insert(heap_t **root, int value);
+heap_t *array_to_heap(int *array, size_t size);
+int heap_extract(heap_t **root);
+int *heap_to_sorted_array(heap_t *heap, size_t *size);
+void binary_tree_print(const binary_tree_t *);
 
-#endif /* _BINARY_TREES_H_ */
+avl_t *rebalance(avl_t *node, avl_t **tree);
+avl_t *balance_left(avl_t *node);
+avl_t *balance_right(avl_t *node);
+char *convert(unsigned long int num, int base, int lowercase);
+bst_t *swap(bst_t *a, bst_t *b);
+
+size_t _binary_tree_height(const binary_tree_t *tree);
+
+#endif /* BINARY_TREES_H */
