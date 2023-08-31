f51c79a9886a32cac52f27c8e62500925870abca
HamzaFikri <mohammed.hamza.fikri@gmail.comgit config --global user.name HamzaFikrigit config --global user.email mohammed.hamza.fikri@gmail.com>
2023-08-31 05:23:03 -0700

    Header file

--- binary_trees.h
+++ binary_trees.h
@@ -5,9 +5,6 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <math.h>
-#include <string.h>
-#include <unistd.h>
-#include <limits.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -27,8 +24,6 @@ typedef struct binary_tree_s
 
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
-typedef struct binary_tree_s binary_tree_t;
-typedef struct binary_tree_s heap_t;
 
 /**
 * struct node_list - Linkedlist node
@@ -90,12 +85,6 @@ avl_t *avl_insert(avl_t **tree, int value);
 avl_t *array_to_avl(int *array, size_t size);
 avl_t *avl_remove(avl_t *root, int value);
 avl_t *sorted_array_to_avl(int *array, size_t size);
-
-/*------ Heap tasks prototypes----- */
 int binary_tree_is_heap(const binary_tree_t *tree);
-heap_t *heap_insert(heap_t **root, int value)
-heap_t *array_to_heap(int *array, size_t size);
-int heap_extract(heap_t **root);
-int *heap_to_sorted_array(heap_t *heap, size_t *size);
 
 #endif /* _BINARY_TREES_H_ */
